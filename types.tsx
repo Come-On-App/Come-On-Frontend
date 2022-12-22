edc0bd0803f88190fa49bff67ae20e6c688caf7d
jeongbaebang <jeongbaebang_dev@naver.com>
2022-12-22 10:22:42 +0900

    🍕 [styles] : 전역 테마 색상 & 타입 설정 (#13)
    * ⚙️ Config : ESLint Ruls 설정
    
    작업 내용 :
    1. depth 2이하로 제한
    2. 함수 매개변수 3개로 제한
    3. 논리적으로 다른 블록 공백 추가
    
    * ⚙️ Config : package 내용 추가
    
    작업 내용 : 라이센스, 버그리포트, 홈페이지, 키워드 추가
    
    * ⚙️ Config : React Native Elements (UI Kit) 설치
    
    작업 내용 : npm install @rneui/themed @rneui/base
    
    * ⚙️ Config : ESLint ignorePatterns 속성 추가
    
    작업 내용 : "babel.config.js", "node_modules"
    
    * 🎨 Design : create Global Theme types config
    
    작업 내용 : Background Color, Typography, Grayscale
    
    * 🎨 Design : create Global Theme
    
    작업 내용 : Background Color, Typography, Grayscale, bold

--- types.tsx
+++ types.tsx
@@ -4,12 +4,16 @@
  */
 
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
+import {
+  CompositeScreenProps,
+  NavigatorScreenParams,
+} from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import '@rneui/themed';
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
+    type RootParamList = RootStackParamList;
   }
 }
 
@@ -19,17 +23,64 @@ export type RootStackParamList = {
   NotFound: undefined;
 };
 
-export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
-  RootStackParamList,
-  Screen
->;
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, Screen>;
 
 export type RootTabParamList = {
   TabOne: undefined;
   TabTwo: undefined;
 };
 
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
-  BottomTabScreenProps<RootTabParamList, Screen>,
-  NativeStackScreenProps<RootStackParamList>
->;
+export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
+  CompositeScreenProps<
+    BottomTabScreenProps<RootTabParamList, Screen>,
+    NativeStackScreenProps<RootStackParamList>
+  >;
+
+/**
+ * 전역테마 타입
+ */
+type Typography =
+  | 'title1'
+  | 'title2'
+  | 'title3'
+  | 'title4'
+  | 'body1'
+  | 'body2'
+  | 'body3'
+  | 'caption';
+
+type Grayscale =
+  | '50'
+  | '100'
+  | '200'
+  | '300'
+  | '400'
+  | '500'
+  | '600'
+  | '700'
+  | '800'
+  | '900';
+
+declare module '@rneui/themed' {
+  export interface Colors {
+    info: string;
+  }
+  export interface Theme {
+    grayscale: {
+      [Key in Grayscale]: string;
+    };
+    textStyles: {
+      [Key in Typography]: {
+        fontSize: number;
+        lineHeight: number;
+      };
+    };
+  }
+  export interface TextProps {
+    bold?: boolean;
+  }
+  export interface ComponentTheme {
+    Text: Partial<TextProps>;
+  }
+}
