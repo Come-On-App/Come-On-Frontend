43afdb8e5f758f4fb16e0f9096954a2570b5feb7
DESKTOP-PC\USER <bananana0118@gmail.com>
2023-01-01 21:27:47 +0900

    🖌️ Modify : StyledText Font default Props 설정 optional 로 바꿨기 때문에 Font의 defaultProps를 설정해 빨간줄이 뜨지 않게 했습니다.

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text } from '@rneui/themed';
-
+import theme from '../constants/themed';
 import { TextProps } from '../types';
 
 function Font({ style, children }: TextProps) {
@@ -12,3 +12,11 @@ export function PretendardText({ style, children }: TextProps) {
 }
 
 export default Font;
+
+Font.defaultProps = {
+  style: {
+    color: theme.grayscale?.[900],
+    fontSize: theme.textStyles?.title4?.fontSize,
+    lineHeight: theme.textStyles?.title4?.lineHeight,
+  },
+};
