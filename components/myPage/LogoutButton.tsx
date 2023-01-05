c186806a046459c5244dd10ef0c73ab033cb7a6f
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-01-05 23:32:39 +0900

    ➕ Add : 마이페이지 로그아웃 버튼 추가

---
+++ components/myPage/LogoutButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Button, makeStyles } from '@rneui/themed';
+
+function LogoutButton() {
+  const styles = useStyles();
+  const TITLE = '로그아웃';
+
+  return (
+    <View>
+      <Button
+        title={TITLE}
+        type="clear"
+        titleStyle={styles.button}
+        containerStyle={styles.buttonContainer}
+      />
+    </View>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    fontFamily: 'pretendard-regular',
+    fontSize: 12,
+    color: theme.grayscale['500'],
+  },
+  buttonContainer: {
+    marginRight: 8,
+  },
+}));
+
+export default LogoutButton;
