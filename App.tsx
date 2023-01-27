8d3996507a0a27b15a7ab0de24bd2562a2ccbe38
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-01-27 14:40:11 +0900

    ➕ Add : 전역 상태 관리 라이브러리 적용
    #30

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable react/style-prop-object */
+
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '@rneui/themed';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { store } from './app/store';
 import Navigation from './navigation';
 import theme from './constants/themed';
 import useCachedResources from './hooks/useCachedResources';
@@ -19,7 +23,9 @@ export default function App() {
       <StatusBar style="dark" />
       <ThemeProvider theme={theme}>
         <SafeAreaProvider>
-          <Navigation />
+          <Provider store={store}>
+            <Navigation />
+          </Provider>
         </SafeAreaProvider>
       </ThemeProvider>
     </>
