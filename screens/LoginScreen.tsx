44236348fbac4d5993647e14d09e9a2ea730eeac
YeaJeong <bananana0118@gmail.com>
2023-02-02 00:20:48 +0900

    🔨 Refactor : 로그인스크린 리팩토링
    - 로그인 스크린 리팩토링

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,156 +1,46 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import WebView from 'react-native-webview';
+import React, { useState } from 'react';
 import { makeStyles } from '@rneui/themed';
-import { View, Text, Pressable, Button } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import * as Google from 'expo-auth-session/providers/google';
-import { ResponseType } from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
-import {
-  REACT_APP_REST_API_KEY,
-  REACT_APP_REDIRECT_URI,
-  REACT_APP_EXPO_CLIENT_ID,
-  REACT_APP_ANDROID_CLIENT_ID,
-  REACT_APP_WEB_CLIENT_ID,
-  REACT_APP_IOS_CLIENT_ID,
-} from '@env';
-import Logo from '../components/Logo';
-import useAuth from '../hooks/useAuth';
-import AppleLogo from '../assets/images/logo/AppleLogo';
-import KakaoLogo from '../assets/images/logo/KakaoLogo';
-import GoogleLogo from '../assets/images/logo/GoogleLogo';
+import { View, Text, Pressable } from 'react-native';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 
-const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage("check")`;
+import Logo from '../components/Logo';
+import KakaoLoginBtn, {
+  KakaoLoginWebView,
+} from '../components/buttons/KakaoLoginBtn';
+import { RootStackScreenProps } from '../navigation';
+import AppleLogo from '../assets/images/logo/AppleLogo';
+import GoogleLoginBtn from '../components/buttons/GoogleLoginBtn';
 
 WebBrowser.maybeCompleteAuthSession();
 
-function GoogleLoginHandler() {
-  const { setLogin } = useAuth();
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId: REACT_APP_EXPO_CLIENT_ID,
-    iosClientId: REACT_APP_IOS_CLIENT_ID,
-    androidClientId: REACT_APP_ANDROID_CLIENT_ID,
-    webClientId: REACT_APP_WEB_CLIENT_ID,
-    responseType: ResponseType.IdToken,
-  });
-
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { params } = response;
-
-      console.log(response);
-
-      setLogin(JSON.stringify(params));
-    }
-  }, [response, setLogin]);
-
-  return (
-    <Pressable onPress={() => promptAsync()}>
-      <GoogleLogo />
-    </Pressable>
-  );
-}
-
-function TestWebView() {
-  const { setLogin } = useAuth();
-
-  function LogInProgress(data: string) {
-    const exp = 'code=';
-    const condition = data.indexOf(exp);
-
-    if (condition !== -1) {
-      const requestCode = data.substring(condition + exp.length);
-
-      // 토큰값 받기
-      requestToken(requestCode);
-    }
-  }
-
-  const requestToken = async (requestCode: string) => {
-    const requestTokenUrl = 'http://211.204.19.184:8088/api/v1/oauth/kakao';
-
-    await axios({
-      method: 'post',
-      url: requestTokenUrl,
-      data: {
-        code: requestCode,
-      },
-    })
-      .then(async response => {
-        const { data } = response;
-
-        setLogin(data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <SafeAreaView style={{ flex: 1, width: '100%' }}>
-      <WebView
-        originWhitelist={['*']}
-        scalesPageToFit
-        style={{ marginTop: 30 }}
-        source={{
-          uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REACT_APP_REST_API_KEY}&redirect_uri=${REACT_APP_REDIRECT_URI}`,
-        }}
-        injectedJavaScript={INJECTED_JAVASCRIPT}
-        javaScriptEnabled
-        onMessage={event => {
-          LogInProgress(event.nativeEvent.url);
-        }}
-      />
-      <View />
-    </SafeAreaView>
-  );
-}
-
-function LoginScreen() {
+function LoginScreen({ navigation }: RootStackScreenProps<'LoginScreen'>) {
   const styles = useStyles();
   const [visible, setVisible] = useState(false);
 
   return (
     <View style={styles.container}>
       {visible ? (
-        <TestWebView />
+        <KakaoLoginWebView />
       ) : (
         <>
+          <Toast />
           <Logo />
-          <View
-            style={{
-              width: '100%',
-              height: 30,
-              alignItems: 'center',
-            }}
-          >
+          <View style={styles.labels}>
             <Text style={styles.label}>소셜 로그인</Text>
             <View style={styles.line} />
           </View>
-          <View
-            style={{
-              width: '100%',
-              height: '30%',
-              alignItems: 'center',
-              position: 'relative',
-              top: -30,
-              marginBottom: 30,
-            }}
-          >
-            {/** 추후 Pressable 스타일링 추가해서 공통컴포넌트, apple/android에 따른 구분할것 */}
-            <Pressable onPress={() => setVisible(!visible)}>
-              <View style={styles.kakaoLogoStyle}>
-                <KakaoLogo />
-              </View>
-            </Pressable>
-
-            <View style={styles.appleLogoStyle}>
+          <View style={styles.buttons}>
+            <KakaoLoginBtn setVisible={setVisible} />
+            <Pressable
+              style={({ pressed }) => [
+                pressed && styles.pressed,
+                styles.btnStyle,
+              ]}
+            >
               <AppleLogo />
-            </View>
-            <View style={styles.googleLogoStyle}>
-              <GoogleLoginHandler />
-            </View>
+            </Pressable>
+            <GoogleLoginBtn />
           </View>
         </>
       )}
@@ -185,43 +75,19 @@ const useStyles = makeStyles(theme => ({
     marginBottom: 3,
     color: theme.grayscale[400],
   },
-  kakaoLogoStyle: {
-    width: '80%',
-    flexDirection: 'row',
+  btnStyle: {
+    marginBottom: 12,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  buttons: {
+    width: '100%',
+    height: '30%',
     alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
-    backgroundColor: '#FEE500',
-    borderRadius: 12,
-    margin: 5,
+    position: 'relative',
+    top: -30,
+    marginBottom: 30,
   },
-  appleLogoStyle: {
-    width: '80%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
-    backgroundColor: '#000000',
-    margin: 5,
-    borderRadius: 12,
-  },
-  googleLogoStyle: {
-    width: '80%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
-    backgroundColor: '#ffffff',
-    margin: 5,
-    borderRadius: 12,
-  },
-  appleText: {
-    color: 'white',
-  },
-  googleText: {
-    color: 'white',
-  },
-  kakaoText: {
-    color: 'white',
-  },
+  labels: { width: '100%', height: 30, alignItems: 'center' },
 }));
