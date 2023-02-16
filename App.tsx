b1f0356d9bbf8c292375b3328764fad753f32fcb
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-02-16 20:23:02 +0900

    ➕ Add : react - query 구성 파일 추가

--- App.tsx
+++ App.tsx
@@ -4,17 +4,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '@rneui/themed';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { store } from './src/app/store';
 import Navigation from './src/navigation';
 import theme from './src/constants/themed';
+import { queryClient } from './src/api/queryClient';
 import useCachedResources from './src/hooks/useCachedResources';
-import { store } from './src/app/store';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const queryClient = new QueryClient();
 
   if (!isLoadingComplete) {
     return null;
@@ -24,13 +24,13 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <SafeAreaProvider>
-            <Provider store={store}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <SafeAreaProvider>
               <Navigation />
-            </Provider>
-          </SafeAreaProvider>
-        </ThemeProvider>
+            </SafeAreaProvider>
+          </ThemeProvider>
+        </Provider>
       </QueryClientProvider>
     </>
   );
---
+++ src/api/queryClient.ts
@@ -0,0 +1,38 @@
+import { QueryClient } from 'react-query';
+
+export enum QueryKeys {
+  user = 'user',
+  meeting = 'meeting',
+}
+
+export const queryClient = new QueryClient({});
+
+export const updateCache = <Payload>(queryKey: QueryKeys, payalod: Payload) => {
+  queryClient.setQueryData(queryKey, payalod);
+};
+
+export const getCache = <Payload>(queryKey: QueryKeys) => {
+  return queryClient.getQueryData<Payload>(queryKey);
+};
+
+export const cancelQueries = (queryKey: QueryKeys) => {
+  queryClient.cancelQueries(queryKey);
+};
+
+export const useQueryCache = <Payalod>(queryKey: QueryKeys) => {
+  const updateQueryCache = (payload: Partial<Payalod>) => {
+    updateCache(queryKey, payload);
+  };
+  const getQueryCache = () => {
+    return getCache<Payalod>(queryKey);
+  };
+  const cancelQueryCache = () => {
+    cancelQueries(queryKey);
+  };
+
+  return {
+    updateQueryCache,
+    getQueryCache,
+    cancelQueryCache,
+  };
+};
