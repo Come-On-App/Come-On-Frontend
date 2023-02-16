c00c43d736ee183c23978db0096cf6b5f23d81b7
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-02-16 22:43:25 +0900

    ➕ Add : Image API 추가

---
+++ src/api/image/upload.ts
@@ -0,0 +1,45 @@
+import jwt from '@api/jwt';
+import { promiseFlow } from '@utils/promise';
+import { getJWTHeader, serverAxios } from '@api/axiosInstance';
+import { convertImageFormData, createImageFormData } from '@utils/image';
+
+import type {
+  PostUploadImagePayload,
+  PostUploadImageResponse,
+} from '@type/api.image';
+import { AssetState } from '@type/hook.imagePicker';
+
+function getImageUrl(item: PostUploadImageResponse) {
+  return item.imageUrl;
+}
+
+/**
+ * POST /api/v1/image 이미지 업로드
+ * @requires Authorization Bearer {access-token}
+ * @param payload 업로드할 이미지 파일
+ * @returns 업로드된 이미지의 URL
+ */
+export async function requestUploadImage(
+  payload: PostUploadImagePayload,
+): Promise<PostUploadImageResponse> {
+  const URL = '/api/v1/image';
+  const { data } = await serverAxios.post(URL, payload, {
+    headers: {
+      ...getJWTHeader(jwt),
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  return data;
+}
+
+export async function requestImageUpload(
+  imagePath: AssetState,
+): Promise<string> {
+  return promiseFlow(imagePath, [
+    convertImageFormData,
+    createImageFormData,
+    requestUploadImage,
+    getImageUrl,
+  ]);
+}
---
+++ src/types/hook.imagePicker.ts
@@ -0,0 +1,6 @@
+export type AssetState = {
+  name: string;
+  type: string;
+  uri: string;
+  base64?: string | null | undefined;
+};
