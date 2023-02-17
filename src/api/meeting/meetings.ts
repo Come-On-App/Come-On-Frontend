cc210b7ab6b33f80c39f1878695242fbd2cdb2bc
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-02-17 13:06:07 +0900

    ➕ Add : 모임 API 추가

---
+++ src/api/meeting/meetings.ts
@@ -0,0 +1,154 @@
+import jwt from '@api/jwt';
+import { getJWTHeader, serverAxios } from '@api/axiosInstance';
+import type {
+  GetEntryCodePayload,
+  GetEntryCodeResponse,
+  GetMeetingDetailPayload,
+  GetMeetingDetailResponse,
+  GetMeetingPayload,
+  GetMeetingSliceResponse,
+  PostEntryCodePayalod,
+  PostEntryCodeResponse,
+  PostJoinPayload,
+  PostJoinResponse,
+  PostMeetingPayload,
+  PostMeetingResponse,
+  PostMeetingTimePayalod,
+  PostMeetingTimeResponse,
+} from '@type/api.meeting';
+
+/**
+ * POST /api/v1/meetings 모임 등록
+ * @requires Authorization Bearer {access-token}
+ * @param payload 모임 생성에 필요한 정보를 전달한다.
+ * @returns 생성된 모임의 식별값
+ */
+export async function requestCreateMeetings(
+  payload: PostMeetingPayload,
+): Promise<PostMeetingResponse> {
+  const URL = '/api/v1/meetings';
+  const { data } = await serverAxios.post(URL, payload, {
+    headers: {
+      ...getJWTHeader(jwt),
+    },
+  });
+
+  return data;
+}
+
+// TODO: PATCH /api/v1/meetings/{meeting-id} 모임 수정
+
+/**
+ * GET /api/v1/meetings 모임 리스트 조회
+ * @requires Authorization Bearer {access-token}
+ * @param payload 모임 리스트 필터링 옵션 (필수값 X)
+ * @returns slice Response 형식 응답값
+ */
+export async function requestGetMeetings(
+  payload?: Partial<GetMeetingPayload>,
+): Promise<GetMeetingSliceResponse> {
+  const URL = '/api/v1/meetings';
+  const { data } = await serverAxios.get(URL, {
+    params: payload,
+    headers: {
+      ...getJWTHeader(jwt),
+    },
+  });
+
+  return data;
+}
+
+/**
+ * GET /api/v1/meetings/{meeting-id} 모임 상세 조회
+ * @requires Authorization Bearer {access-token}
+ * @param payload 조회할 모임의 식별값
+ * @returns 모임 상세 정보
+ */
+export async function requestGetMeetingDetail(
+  payload: GetMeetingDetailPayload,
+): Promise<GetMeetingDetailResponse> {
+  const URL = `/api/v1/meetings/${payload}`;
+  const { data } = await serverAxios.get(URL, {
+    headers: {
+      ...getJWTHeader(jwt),
+    },
+  });
+
+  return data;
+}
+
+/**
+ * POST /api/v1/meetings/join 모임 가입
+ * @requires Authorization Bearer {access-token}
+ * @param payload 모임 입장 코드
+ * @returns 모임 정보
+ */
+export async function requestMeetingJoin(
+  payload: PostJoinPayload,
+): Promise<PostJoinResponse> {
+  const URL = `/api/v1/meetings/join`;
+  const { data } = await serverAxios.post(URL, payload, {
+    headers: {
+      ...getJWTHeader(jwt),
+    },
+  });
+
+  return data;
+}
+
+/**
+ * GET /api/v1/meetings/{meeting-id}/entry-code 모임 입장 코드 조회
+ * @requires Authorization Bearer {access-token}
+ * @param payload 입장 코드를 조회할 모임의 식별값
+ * @returns 모임 입장 코드 정보
+ */
+export async function requestGetEntryCode(
+  payload: GetEntryCodePayload,
+): Promise<GetEntryCodeResponse> {
+  const URL = `/api/v1/meetings/${payload}/entry-code`;
+  const { data } = await serverAxios.get(URL, {
+    headers: {
+      ...getJWTHeader(jwt),
+    },
+  });
+
+  return data;
+}
+
+/**
+ * POST /api/v1/meetings/{meeting-id}/entry-code 모임 입장 코드 갱신
+ * @requires Authorization Bearer {access-token}
+ * @param payload 입장 코드를 조회할 모임의 식별값
+ * @returns 모임 입장 코드 정보
+ */
+export async function requestPostEntryCode(
+  payload: PostEntryCodePayalod,
+): Promise<PostEntryCodeResponse> {
+  const URL = `/api/v1/meetings/${payload}/entry-code`;
+  const { data } = await serverAxios.post(URL, {
+    headers: {
+      ...getJWTHeader(jwt),
+    },
+  });
+
+  return data;
+}
+
+/**
+ * POST /api/v1/meetings/{meeting-id}/meeting-time 모임 시간 변경
+ * @requires Authorization Bearer {access-token}
+ * @param payload 모임 시간을 변경할 모임의 식별값
+ * @returns 요청 처리 성공 여부
+ */
+export async function requestPostMeetingTime(
+  payload: PostMeetingTimePayalod,
+): Promise<PostMeetingTimeResponse> {
+  const URL = `/api/v1/meetings/${payload.meetingId}/entry-code`;
+  const { data } = await serverAxios.post(URL, payload.meetingStartTime, {
+    headers: {
+      ...getJWTHeader(jwt),
+    },
+  });
+
+  return data;
+}
