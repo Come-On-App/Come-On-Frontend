9b2fd763eebfc06af9905c934be7e01d4decad77
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-02-19 15:49:48 +0900

    ➕ Add : 유틸 함수 추가

---
+++ src/utils/log.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+
+function GenerateLog(
+  type: 'log' | 'warn' | 'error' | 'table',
+  option?: {
+    time: boolean;
+  },
+) {
+  const print = console[type];
+
+  return <T>(title: string, value: T) => {
+    if (option?.time) {
+      const time = new Date().toLocaleString();
+
+      return print(`[${time}] ${title}:`, value);
+    }
+
+    return print(`${title}`, value);
+  };
+}
+
+export default GenerateLog;
