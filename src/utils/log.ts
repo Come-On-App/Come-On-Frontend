db504aac818755a18d82e54292f8829798860e74
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-02-19 16:42:49 +0900

    ➕ Add : log 유틸 함수 옵션 속성 추가

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -4,15 +4,18 @@ function GenerateLog(
   type: 'log' | 'warn' | 'error' | 'table',
   option?: {
     time: boolean;
+    hidden: boolean;
   },
 ) {
   const print = console[type];
 
   return <T>(title: string, value: T) => {
+    if (option?.hidden) return undefined;
+
     if (option?.time) {
       const time = new Date().toLocaleString();
 
-      return print(`[${time}] ${title}:`, value);
+      return print(`[${time}] ${title}`, value);
     }
 
     return print(`${title}`, value);
