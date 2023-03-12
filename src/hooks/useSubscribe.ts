553f626f842d39732c5fd76e3a09caf4de137bdf
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-03-12 19:21:21 +0900

    ➕ Add : 커스텀 훅 추가

---
+++ src/hooks/useSubscribe.ts
@@ -0,0 +1,203 @@
+import { requestGetMyInfo } from '@api/user/user';
+import { useMemo, useEffect } from 'react';
+import { requestMeetingMembers } from '@api/meeting/members';
+import { invalidateQueries, QueryKeys } from '@api/queryClient';
+import { IMessage } from '@stomp/stompjs';
+import {
+  GetMeetingMembersListResponse,
+  GetMeetingMembersResponse,
+} from '@type/api.meeting';
+import { Individual, IMeeting, IMeetingPlaceLock } from '@type/hook.webSocket';
+import { successAlert } from '@utils/alert';
+import fn, { pickSafelyBy } from '@utils/fn';
+import { log } from '@utils/log';
+import { promiseFlow } from '@utils/promise';
+import { GetMyInfoResponse } from '@type/api.user';
+import { requestPostMeetingPlacesUnLock } from '@api/meeting/places';
+import { PlaceLock } from '@features/placeLockSlice';
+import { fallbackImage } from './query/useUserQuery';
+import usePlace from './redux/usePlace';
+import usePlaceLock, {
+  PlaceLockDispatch,
+  PlaceUnLockDispatch,
+} from './redux/usePlaceLock';
+import useWebSocket from './useWebSocket';
+
+export default function useSubscribe(meetingId: number) {
+  const { placeResetDispatch } = usePlace();
+  const { placeUnLockDispatch } = usePlaceLock();
+
+  useSubscribeIndividual(meetingId);
+  useSubscribePlace(meetingId);
+
+  useEffect(() => {
+    return () => {
+      placeResetDispatch();
+      placeUnLockDispatch();
+    };
+  }, [placeResetDispatch, placeUnLockDispatch]);
+}
+
+// 개인 구독
+function useSubscribeIndividual(meetingId: number) {
+  const { placeLockDispatch } = usePlaceLock();
+  const { subscribeIndividual } = useWebSocket();
+  const onMessage = useMemo(
+    () => onIndividualMessageFn(meetingId, [placeLockDispatch]),
+    [meetingId, placeLockDispatch],
+  );
+
+  useEffect(() => {
+    const subIndividual = subscribeIndividual(meetingId, onMessage);
+
+    return () => {
+      if (!subIndividual) return;
+
+      subIndividual.unsubscribe();
+    };
+  }, [meetingId, onMessage, subscribeIndividual]);
+}
+
+// 모임 구독
+function useSubscribePlace(meetingId: number) {
+  const { placeLockDispatch, placeUnLockDispatch } = usePlaceLock();
+  const { placeResetDispatch } = usePlace();
+  const { subscribePlace } = useWebSocket();
+  const onMessage = useMemo(
+    () => onPlaceMessageFn(meetingId, [placeLockDispatch, placeUnLockDispatch]),
+    [meetingId, placeLockDispatch, placeUnLockDispatch],
+  );
+
+  useEffect(() => {
+    const subPlace = subscribePlace(meetingId, onMessage);
+
+    return () => {
+      if (!subPlace) return;
+
+      subPlace.unsubscribe();
+    };
+  }, [meetingId, onMessage, placeResetDispatch, subscribePlace]);
+}
+
+function onIndividualMessageFn(
+  meetingId: number,
+  dispatchs: [PlaceLockDispatch],
+) {
+  const [placeLockDispatch] = dispatchs;
+
+  return (message: IMessage) => {
+    const messageBody: Individual = JSON.parse(message.body);
+
+    log(`/user/queue/meetings/${meetingId} - message.body]`, messageBody);
+
+    if (fn.isEmpty(messageBody.lockedPlaces)) return;
+
+    const [lockedPlace] = messageBody.lockedPlaces;
+
+    promiseFlow(requestGetMyInfo, [
+      ({ userId }: GetMyInfoResponse) => {
+        // 기존에 락을 건 유저가 현재 유저 아이디와 같은 경우 (비정상 경로)
+        if (userId === lockedPlace.lockingUserId) {
+          requestPostMeetingPlacesUnLock({
+            meetingId,
+            placeId: lockedPlace.meetingPlaceId,
+          });
+
+          return;
+        }
+
+        const payload = {
+          data: {
+            meetingId: messageBody.meetingId,
+            meetingPlaceId: lockedPlace.meetingPlaceId,
+            userId: lockedPlace.lockingUserId,
+          },
+        };
+
+        setCurrentLockUser(payload, placeLockDispatch);
+      },
+    ]);
+  };
+}
+
+function onPlaceMessageFn(
+  meetingId: number,
+  dispatchs: [PlaceLockDispatch, PlaceUnLockDispatch],
+) {
+  const [placeLockDispatch, placeUnLockDispatch] = dispatchs;
+
+  return (message: IMessage) => {
+    const messageBody: IMeeting = JSON.parse(message.body);
+
+    log(`[/sub/meetings/${meetingId} - message.body]`, messageBody);
+
+    if (messageBody.messageType === 'RESOURCE_UPDATED_EVENT') {
+      switch (messageBody.data.meetingResourceType) {
+        // 모임 장소 리스트 갱신 메시지
+        case 'MEETING_PLACES':
+          invalidateQueries([QueryKeys.place, meetingId]);
+          successAlert('모임 장소 리스트 갱신됨');
+          break;
+        // 모임 장소 락 등록 메시지
+        case 'MEETING_PLACE_LOCK':
+          setCurrentLockUser(
+            messageBody as IMeetingPlaceLock,
+            placeLockDispatch,
+          );
+          break;
+        // 모임 장소 락 해제 메시지
+        case 'MEETING_PLACE_UNLOCK':
+          placeUnLockDispatch();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+}
+
+interface Props {
+  data: {
+    meetingId: number;
+    meetingPlaceId: number;
+    userId: number;
+  };
+}
+
+function setCurrentLockUser(
+  { data: messageBody }: Props,
+  dispatch: PlaceLockDispatch,
+) {
+  const payload: PlaceLock = {
+    meetingResourceType: 'MEETING_PLACE_LOCK',
+    meetingId: messageBody.meetingId,
+    meetingPlaceId: messageBody.meetingPlaceId,
+    userId: messageBody.userId,
+    lockUserImage: fallbackImage,
+  };
+
+  // @see [https://github.com/Come-On-App/Come-On-Frontend/issues/97]
+  dispatch(payload);
+
+  promiseFlow<number, GetMeetingMembersResponse>(
+    messageBody.meetingId,
+    [
+      requestMeetingMembers,
+      (info: GetMeetingMembersListResponse) => {
+        const [currentLockUser] = info.contents.filter(
+          item => item.userId === messageBody.userId,
+        );
+
+        return currentLockUser;
+      },
+    ],
+    {
+      onSuccess: user => {
+        dispatch({
+          ...payload,
+          lockUserImage: pickSafelyBy(user, 'profileImageUrl', fallbackImage),
+        });
+      },
+    },
+  );
+}
