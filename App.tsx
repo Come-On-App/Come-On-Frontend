f5fcb38321dfdf7df3d9e10dd35146f019c46f50
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-03-12 18:51:23 +0900

    🖌️ Modify : react-navite react-query 설정

--- App.tsx
+++ App.tsx
@@ -1,22 +1,39 @@
 /* eslint-disable react/style-prop-object */
 
+import * as encoding from 'text-encoding';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
-import { QueryClientProvider } from 'react-query';
+import { focusManager, QueryClientProvider } from 'react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import { ThemeProvider } from '@rneui/themed';
+import { AppStateStatus, Platform } from 'react-native';
 
+import useOnlineManager from '@hooks/query/useOnlineManager';
+import useAppState from '@hooks/query/useAppState';
 import { store } from './src/app/store';
 import Navigation from './src/navigation';
 import { queryClient } from './src/api/queryClient';
 import useCachedResources from './src/hooks/useCachedResources';
 import theme from './src/constants/themed';
-import DevMode from './src/experimental/DevMode';
+
+Object.assign(global, {
+  TextEncoder: encoding.TextEncoder,
+  TextDecoder: encoding.TextDecoder,
+});
+
+function onAppStateChange(status: AppStateStatus) {
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active');
+  }
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
+
+  useOnlineManager();
+  useAppState(onAppStateChange);
 
   if (!isLoadingComplete) {
     return null;
@@ -30,7 +47,6 @@ export default function App() {
           <ThemeProvider theme={theme}>
             <SafeAreaProvider>
               <Navigation />
-              <DevMode />
             </SafeAreaProvider>
           </ThemeProvider>
         </Provider>
