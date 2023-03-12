dc9f25dc59df16d46c626a157c90a14e2988ed50
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-03-12 19:05:36 +0900

    🖌️ Modify : 캔슬 버튼 컴포넌트 리팩토링

---
+++ src/components/button/CancelButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleProp, StyleSheet, TextStyle, View } from 'react-native';
+import { Button } from '@rneui/themed';
+import theme from '../../constants/themed';
+
+interface ButtonProps {
+  title: string;
+  onPressHandler: () => void;
+  style?: StyleProp<TextStyle>;
+}
+
+// useNavigation();
+
+function CancelButton({ title, onPressHandler, style }: ButtonProps) {
+  return (
+    <View style={style}>
+      <Button
+        title={title}
+        onPress={onPressHandler}
+        buttonStyle={[styles.cancelBtnStyle]}
+      />
+    </View>
+  );
+}
+
+export default CancelButton;
+
+const styles = StyleSheet.create({
+  cancelBtnStyle: {
+    minWidth: 150,
+    borderRadius: 4,
+    height: 48,
+    backgroundColor: theme.grayscale?.[300],
+    color: 'white',
+  },
+});
+
+CancelButton.defaultProps = {
+  style: styles.cancelBtnStyle,
+};
---
+++ src/components/button/CancelIconButton.tsx
@@ -0,0 +1,65 @@
+import React, { useCallback } from 'react';
+import { StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { CancelIconButtonWithActionProps } from '@type/component.button';
+import { log } from '@utils/log';
+import { requestPostMeetingPlacesUnLock } from '@api/meeting/places';
+import { promiseFlow } from '@utils/promise';
+import usePlace from '@hooks/redux/usePlace';
+
+export default function CancelIconButton() {
+  const navigation = useNavigation();
+
+  return (
+    <MaterialIcons
+      style={styles.cancelIcon}
+      name="clear"
+      onPress={() => navigation.goBack()}
+    />
+  );
+}
+
+export function CancelIconButtonWithAction({
+  actionFn,
+}: CancelIconButtonWithActionProps) {
+  const navigation = useNavigation();
+  const onPressHandler = useCallback(() => {
+    actionFn();
+    navigation.goBack();
+  }, [actionFn, navigation]);
+
+  return (
+    <MaterialIcons
+      style={styles.cancelIcon}
+      name="clear"
+      onPress={onPressHandler}
+    />
+  );
+}
+
+export function CancelPlaceSelectIconButton() {
+  const { placeResetDispatch, placeState } = usePlace();
+  const actionFn = () => {
+    if (placeState.state === 'Modify' && placeState.isLock) {
+      const payload = {
+        meetingId: placeState.meetingId,
+        placeId: placeState.meetingPlaceId,
+      };
+
+      promiseFlow(payload, [requestPostMeetingPlacesUnLock], {
+        onSuccess: placeResetDispatch,
+      });
+    }
+  };
+
+  return <CancelIconButtonWithAction actionFn={actionFn} />;
+}
+
+const styles = StyleSheet.create({
+  cancelIcon: {
+    color: 'black',
+    fontSize: 24,
+    justifyContent: 'center',
+  },
+});
