4987b608c7116c46d46efdecb35ba2c6337d4625
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-03-12 19:38:54 +0900

    🖌️ Modify : 네비게이션 로직 수정 & 웹소켓 연결

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -11,34 +11,45 @@ import type {
   RootStackParamList,
   RootTabParamList,
 } from '@type/navigation';
-import { useUser } from '@hooks/useUser';
+
+import MeetingDetail from '@screens/meeting/detail';
+import { getValueFor } from '@utils/secureStore';
+import { useWebSocketConnect } from '@hooks/useWebSocket';
+import useUserQuery from '@hooks/query/useUserQuery';
+import fn from '@utils/fn';
+import { Skeleton } from '@rneui/themed';
 import theme from '../constants/themed';
 
-import apis from '../api';
 import useAuth from '../hooks/useAuth';
 import MeetingRoom from '../screens/MeetingRoom';
 import LoginScreen from '../screens/LoginScreen';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
+import TabOneScreen from '../screens/bottomTap/TabOneScreen';
+import TabTwoScreen from '../screens/bottomTap/TabTwoScreen';
 import CreateMeeting from '../screens/CreateMeeting';
-import TabThreeScreen from '../screens/TabThreeScreen';
+import TabThreeScreen from '../screens/bottomTap/TabThreeScreen';
 import CreateMeetingCalender from '../screens/CreateMeetingCalender';
 
 import LogoutButton from '../components/myPage/MyPageLogoutButton';
-import { createTabBarIcon, PressableIcon } from '../components/Icon';
+import { createTabBarIcon } from '../components/Icon';
 
-import CancelIconButton from '../components/buttons/CancelIconButton';
+import CancelIconButton, {
+  CancelPlaceSelectIconButton,
+} from '../components/button/CancelIconButton';
 import MyPageHeaderTitle from '../components/myPage/MyPageHeaderTitle';
 import PlaceSelectHeaderTitle from '../components/placeSelect/PlaceSelectHeaderTitle';
 
-import PlaceSelect from '../screens/place/PlaceSelect';
-import PlaceSearch from '../screens/place/PlaceSearch';
+import PlaceSelect from '../screens/place/Select';
+import PlaceSearch from '../screens/place/Search';
 
 function TabThreeIcon() {
-  const { user } = useUser();
   const size = 32;
+  const { user } = useUserQuery();
 
-  return <Avatar size={size} path={user?.profileImageUrl} />;
+  if (fn.isEmpty(user)) {
+    return <Skeleton circle width={size} height={size} />;
+  }
+
+  return <Avatar size={size} path={user.profileImageUrl} />;
 }
 
 export default function Navigation() {
@@ -58,13 +69,14 @@ function PlaceSelectNavigator() {
       <PlaceSelectStack.Screen
         name="Main"
         component={PlaceSelect}
-        options={() => ({
+        options={{
           headerTitleAlign: 'center',
           headerTitle: PlaceSelectHeaderTitle,
           headerShadowVisible: false,
-          headerRight: CancelIconButton,
+          headerLeft: () => null,
           headerBackVisible: false,
-        })}
+          headerRight: CancelPlaceSelectIconButton,
+        }}
       />
       <PlaceSelectStack.Screen name="Map" component={PlaceSearch} />
     </PlaceSelectStack.Navigator>
@@ -73,11 +85,28 @@ function PlaceSelectNavigator() {
 
 function RootNavigator() {
   const { isAuth: isLogin, setLogoin } = useAuth();
+  const [tk, stk] = useState<string>();
 
+  // FIXME: 로직 수정 (임시 토큰 조회 로직)
   useEffect(() => {
     setLogoin(); // 토큰이 있는지 없는지 검사
-    apis.getUser();
-  }, [setLogoin, isLogin]);
+  }, [setLogoin]);
+
+  useEffect(() => {
+    if (!isLogin) return;
+
+    (async () => {
+      const info = await getValueFor('accessToken');
+
+      if (info) {
+        const { token } = JSON.parse(info);
+
+        stk(token);
+      }
+    })();
+  }, [isLogin]);
+
+  useWebSocketConnect(tk);
 
   return (
     <Stack.Navigator>
@@ -91,7 +120,19 @@ function RootNavigator() {
           <Stack.Screen
             name="PlaceSelect"
             component={PlaceSelectNavigator}
-            options={{ headerShown: false }}
+            options={{
+              headerShown: false,
+              gestureEnabled: false,
+            }}
+          />
+          {/* FIXME: 개발용 */}
+          <Stack.Screen
+            name="MeetingDetail"
+            component={MeetingDetail}
+            options={{
+              headerShown: true,
+              contentStyle: { backgroundColor: '#ffffff' },
+            }}
           />
           <Stack.Screen
             name="CreateMeeting"
@@ -171,22 +212,8 @@ function BottomTabNavigator() {
         <BottomTab.Screen
           name="TabTwo"
           component={TabTwoScreen}
-          options={({ navigation }) => ({
-            headerLeft: () =>
-              PressableIcon({
-                name: 'sensor-door',
-                size: 32,
-                color: 'black',
-                onPress: () => navigation.navigate('MeetingRoom'),
-              }),
-            headerRight: () =>
-              PressableIcon({
-                name: 'add',
-                size: 32,
-                color: 'black',
-                onPress: () =>
-                  navigation.navigate('CreateMeeting', { title: '모임생성' }),
-              }),
+          options={() => ({
+            headerShown: false,
             tabBarLabel: '모임입장',
             tabBarIcon: createTabBarIcon('meeting-room'),
           })}
