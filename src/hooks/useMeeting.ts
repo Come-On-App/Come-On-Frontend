96df202c203eb97e26f7d96c41ca918f8fc00c68
YeaJeong <bananana0118@gmail.com>
2023-03-13 10:16:41 +0900

    🔨 Refactor: 코드 리팩토링
    - 커스텀 훅 안에 기능 추가

--- src/hooks/useMeeting.ts
+++ src/hooks/useMeeting.ts
@@ -1,6 +1,9 @@
 import { useCallback } from 'react';
+import { setMeetingName } from '@features/meetingSlice';
+import { AssetState } from '@type/hook.imagePicker';
 import {
   setMeetingId,
+  setMeetingImgPath,
   resetMeetingData as reset,
 } from '../features/meetingSlice';
 import { useAppSelector, useAppDispatch } from '../app/hooks';
@@ -8,6 +11,7 @@ import { useAppSelector, useAppDispatch } from '../app/hooks';
 function useMeeting() {
   const dispatch = useAppDispatch();
   const meetingId = useAppSelector(state => state.meeting.meetingId);
+  const meetingImgPath = useAppSelector(state => state.meeting.meetingImgPath);
   const meetingData = useAppSelector(state => state.meeting.meetingData);
   const setCurrentMeetingId = (meetId: number) => {
     dispatch(setMeetingId(meetId));
@@ -15,11 +19,20 @@ function useMeeting() {
   const resetMeetingData = useCallback(() => {
     dispatch(reset());
   }, [dispatch]);
+  const setMyMeetingName = (text: string) => {
+    dispatch(setMeetingName(text));
+  };
+  const setMyMeetingImgPath = (path: AssetState) => {
+    dispatch(setMeetingImgPath(path));
+  };
 
   return {
     meetingId,
     meetingData,
+    meetingImgPath,
     setCurrentMeetingId,
+    setMyMeetingImgPath,
+    setMyMeetingName,
     resetMeetingData,
   };
 }
