972cc30aae45f7201cb51852eb363fd0a276a38e
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-03-22 14:43:34 +0900

    🖌️ Modify : 멤버 컴포넌트 로직 수정

--- src/screens/meeting/detail/Member.tsx
+++ src/screens/meeting/detail/Member.tsx
@@ -1,35 +1,23 @@
 import React, { memo } from 'react';
 import Layout from '@components/Layout';
 import MemberBox from '@components/member/MemberBox';
-import { requestMeetingMembers } from '@api/meeting/members';
-import { useQuery } from 'react-query';
-import { QueryKeys } from '@api/queryClient';
+
+import useMemberQuery, { findHostUser } from '@hooks/query/useMemberQuery';
 
 // 모임 멤버
 function Member({ meetingId }: { meetingId: number }) {
-  const { data: meetingMembers } = useQuery(
-    [QueryKeys.members, meetingId],
-    ({ signal }) => requestMeetingMembers(meetingId, signal),
-    {
-      enabled: Boolean(meetingId),
-    },
-  );
+  const { members } = useMemberQuery(meetingId);
 
-  if (!meetingMembers) {
+  if (!members) {
     return null;
   }
-
-  // find host user
-  const [{ userId: hostId }] = meetingMembers.contents.filter(
-    content => content.memberRole === 'HOST',
-  );
 
   return (
     <Layout>
       <MemberBox
         meetingId={meetingId}
-        hostId={hostId}
-        meetingUsers={meetingMembers.contents}
+        hostId={findHostUser(members.contents).userId}
+        meetingUsers={members.contents}
       />
     </Layout>
   );
