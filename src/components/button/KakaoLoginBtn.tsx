b1f22aaeb9212ca7d73cc61c44e2747ce95a7704
YeaJeong <bananana0118@gmail.com>
2023-03-27 19:33:58 +0900

    🖌️ Modify : 카카오 로그인 로직 개선
    - webView 를 강제로 띄우는 방식 => WebBrowser 컴포넌트를 이용

--- src/components/button/KakaoLoginBtn.tsx
+++ src/components/button/KakaoLoginBtn.tsx
@@ -2,17 +2,47 @@ import React from 'react';
 import { makeStyles } from '@rneui/themed';
 import { Pressable } from 'react-native';
 
-import { useNavigation } from '@react-navigation/native';
+import * as WebBrowser from 'expo-web-browser';
+import * as Linking from 'expo-linking';
+import useAuth from '@hooks/useAuth';
+import { SocialLoginProps } from '@type/index';
+import { setLogin } from '@api/auth/auth';
+import { REACT_APP_REST_API_KEY, REACT_APP_REDIRECT_URI } from '@env';
 import KakaoLogo from '../../assets/images/logo/KakaoLogo';
 
 export default function KakaoLoginBtn() {
   const styles = useStyles();
-  const navigation = useNavigation();
+  const { setLogin: login } = useAuth();
+  const baseUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REACT_APP_REST_API_KEY}&redirect_uri=${REACT_APP_REDIRECT_URI}&response_type=code&`;
+  const openBrowserAsync = async () => {
+    const callbackUrl = Linking.createURL('/oauth/callback/kakao?code=');
+    const response = await WebBrowser.openAuthSessionAsync(
+      baseUrl,
+      callbackUrl,
+    );
+
+    if (response.type === 'success') {
+      const exp = 'code=';
+      const condition = response.url.indexOf(exp);
+      const resquestCode = response.url.substring(condition + exp.length);
+
+      await requestToken(resquestCode);
+    }
+  };
+  const requestToken = async (requestCode: string) => {
+    const requestTokenUrl = '/api/v1/oauth/kakao';
+    const data: SocialLoginProps = {
+      url: requestTokenUrl,
+      data: { code: requestCode },
+    };
+
+    setLogin(data).then(res => login(res));
+  };
 
   return (
     <Pressable
       style={({ pressed }) => [pressed && styles.pressed, styles.btnStyle]}
-      onPress={() => navigation.navigate('KakaoLoginWebView')}
+      onPress={openBrowserAsync}
     >
       <KakaoLogo />
     </Pressable>
