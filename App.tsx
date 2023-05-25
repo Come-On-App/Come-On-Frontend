171b4bd1917971d0b29d3912f706803deb1d7fa9
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-05-25 20:24:27 +0900

    Add: 주석 추가

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/**
+ * 작업 목표
+ * 1. 네비게이터 구조화
+ * 2. 네비게이터 테스트 작성
+ * 3. 네비게이터 리팩토링
+ */
+
 export default function App() {
   return (
     <View style={styles.container}>
