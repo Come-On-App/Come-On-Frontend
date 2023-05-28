8ddee335f9f53742c84d68f29cf827f6ff283ec4
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-05-28 21:19:48 +0900

    Add: 테스트 코드 추가

--- modules/app/navigation/BottomTabNavigator.spec.tsx
+++ modules/app/navigation/BottomTabNavigator.spec.tsx
@@ -4,12 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import TestId from '@shared/constants/testIds';
 import BottomTabNavigator from './BottomTabNavigator';
+import { Tab } from './config';
 
 describe('<BottomTabNavigator />', () => {
-  test('첫 번째 하단 네비게이터 마이페이지가 렌더링 된다.', () => {
+  test('세 번째 하단 네비게이터는 마이페이지 컴포넌트가 렌더링 된다.', () => {
     const Navigation = (
       <NavigationContainer>
-        <BottomTabNavigator />
+        <BottomTabNavigator initialRouteName={Tab.three} />
       </NavigationContainer>
     );
 
@@ -20,5 +21,21 @@ describe('<BottomTabNavigator />', () => {
     expect(Component).toBeDefined();
     expect(Component).toBeOnTheScreen();
     expect(Component).toHaveTextContent('MyPage');
+  });
+
+  test('두 번째 하단 네비게이터는 모임 입장 컴포넌트가 렌더링 된다.', () => {
+    const Navigation = (
+      <NavigationContainer>
+        <BottomTabNavigator initialRouteName={Tab.two} />
+      </NavigationContainer>
+    );
+
+    render(Navigation);
+
+    const Component = screen.getByTestId(TestId.join.Meeting);
+
+    expect(Component).toBeDefined();
+    expect(Component).toBeOnTheScreen();
+    expect(Component).toHaveTextContent('EnterMeeting');
   });
 });
