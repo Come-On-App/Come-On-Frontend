b225bd4e590fd9b4ee601e3725fb00821f60c465
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-05-28 21:20:19 +0900

    Modify: 주석 업데이트 & 초기값 수정

--- modules/app/navigation/BottomTabNavigator.tsx
+++ modules/app/navigation/BottomTabNavigator.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import MyPageNavigator from '@account/navigation/MyPageNavigator';
+import EnterMeeting from '@join/screen/EnterMeeting';
 import { Tab, options } from './config';
 import { BottomTabParamList } from '../types/navigation';
 
 const { Navigator, Screen } = createBottomTabNavigator<BottomTabParamList>();
 
-function BottomTabNavigator() {
+/**
+ * 첫번째 스크린: 게시물 리스트
+ * 두번째 스크린: 모임 입장
+ * 세번째 스크린: 마이페이지
+ */
+function BottomTabNavigator({ initialRouteName }: { initialRouteName?: Tab }) {
   return (
-    <Navigator initialRouteName={Tab.three}>
+    <Navigator initialRouteName={initialRouteName}>
+      <Screen
+        name={Tab.two}
+        component={EnterMeeting}
+        options={options[Tab.two]}
+      />
       <Screen
         name={Tab.three}
         component={MyPageNavigator}
