670e6a98e1208fd747011bc3877b05919e33d3a9
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-04 01:30:12 +0900

    Remove: 주석 제거

--- App.tsx
+++ App.tsx
@@ -3,15 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import RootNavigation from './modules/app/navigation/RootNavigation';
 
-/**
- * 작업 목표
- * 0. 프로젝트 구조 구성 (https://www.reactnative.express/app/project_structure) [v]
- * 1. 네비게이터 구조화 [v]
- * 2. 네비게이터 테스트 작성 [v]
- * 3. 네비게이터 리팩토링 [x]
- * 4. 네비게이터 타입작성 [v]
- */
-
 export default function App() {
   return (
     <View style={styles.container}>
