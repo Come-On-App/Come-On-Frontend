986dcaada42592ac714c6f06508ac5904e22df5d
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-05 02:39:37 +0900

    Fix: 텍스트 수정

--- modules/app/navigation/config.ts
+++ modules/app/navigation/config.ts
@@ -6,12 +6,12 @@ export enum Tab {
 
 export const options = {
   [Tab.one]: {
-    tabBarLabel: '모임관리',
+    tabBarLabel: '모임 관리',
   },
   [Tab.two]: {
-    tabBarLabel: '모임입장',
+    tabBarLabel: '모임 입장',
   },
   [Tab.three]: {
-    tabBarLabel: '모임관리',
+    tabBarLabel: '마이페이지',
   },
-};
+} as const;
