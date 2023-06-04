b79bc5975e61f2b5fa7b05cddc8bc5c73177f445
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-04 19:04:51 +0900

    Add: 주석 추가 & 디자인 래퍼 컴포넌트 적용

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { ThemeProvider } from '@rneui/themed';
 import RootNavigation from './modules/app/navigation/RootNavigation';
+import { theme } from './modules/shared/constants/themed';
 
+/**
+ * 목표
+ * 1. 디자인 프레임 워크 도입 [v]
+ * 2. 디자인 레이아웃 반응형 설계 고민 []
+ * 3. 레퍼 컴포넌트 유틸 알아보기 []
+ */
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <RootNavigation />
+      <ThemeProvider theme={theme}>
+        <RootNavigation />
+      </ThemeProvider>
     </View>
   );
 }
