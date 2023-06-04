876f4af4cceafc4c54a73bfb512514d6f4a65dfd
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-05 02:40:23 +0900

    Add: 네비게이터 테스트 코드 추가

--- App.test.tsx
+++ App.test.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { describe, test, expect } from '@jest/globals';
-import { render, screen } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 
 import TestId from '@shared/constants/testIds';
 import App from './App';
+import { options } from './modules/app/navigation/config';
 
 describe('<App />', () => {
-  test('앱이 실행된다면 네비게이터가 올바르게 동작해야 한다.', () => {
-    render(<App />);
+  describe('앱이 실행된다면 네비게이터가 올바르게 동작해야 한다.', () => {
+    test('두 번째 하단 탭을 클릭하면 모임 입장 스크린으로 네비게이터 되어야 한다.', () => {
+      render(<App />);
 
-    const Component = screen.getByTestId(TestId.post.list);
+      const BottomTap = screen.getByRole('button', {
+        name: options.TabTwo.tabBarLabel,
+      });
 
-    expect(Component).toBeOnTheScreen();
-    expect(Component).toHaveTextContent('MeetingPostList');
+      fireEvent.press(BottomTap);
+
+      expect(screen.getByTestId(TestId.userConnection.code)).toBeOnTheScreen();
+    });
+
+    test('세 번째 하단 탭을 클릭하면 마이페이지 스크린으로 네비게이터 되어야 한다.', () => {
+      render(<App />);
+
+      const BottomTap = screen.getByRole('button', {
+        name: options.TabThree.tabBarLabel,
+      });
+
+      fireEvent.press(BottomTap);
+
+      expect(screen.getByTestId(TestId.account.myPage)).toBeOnTheScreen();
+    });
   });
 });
--- modules/app/navigation/RootNavigator.spec.tsx
+++ modules/app/navigation/RootNavigator.spec.tsx
@@ -3,14 +3,18 @@ import { render, screen } from '@testing-library/react-native';
 
 import TestId from '@shared/constants/testIds';
 import RootNavigation from './RootNavigation';
+import { options } from './config';
 
 describe('<RootNavigatro />', () => {
-  test('최초에 앱이 실행되면 모임 게시물 리스트 라우터가 활성화 되어야 한다.', () => {
+  test('최초에 앱이 실행되면 첫 번째 하단 네비게이터가 활성화돼야 한다.', () => {
     render(<RootNavigation />);
 
+    const FirstBottomTap = screen.getByRole('button', {
+      name: options.TabOne.tabBarLabel,
+    });
     const Component = screen.getByTestId(TestId.post.list);
 
     expect(Component).toBeOnTheScreen();
-    expect(Component).toHaveTextContent('MeetingPostList');
+    expect(FirstBottomTap).toHaveAccessibilityState({ selected: true });
   });
 });
