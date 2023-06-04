b7e2778b7996e4025376bb5fc59353001129d246
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-05 00:27:38 +0900

    Add: UIkit 컴포넌트 적용 테스트

--- modules/post/screen/MeetingPostList.spec.tsx
+++ modules/post/screen/MeetingPostList.spec.tsx
@@ -2,11 +2,12 @@ import { describe, expect, test } from '@jest/globals';
 import { render, screen } from '@testing-library/react-native';
 
 import TestId from '@shared/constants/testIds';
+import { ThemeProvider } from '@rneui/themed';
 import MeetingPostList from './MeetingPostList';
 
 describe('<MeetingPostList />', () => {
   test('모임 리스트 컴포넌트가 렌더링 되어야 한다.', () => {
-    render(<MeetingPostList />);
+    render(<MeetingPostList />, { wrapper: ThemeProvider });
 
     const Component = screen.getByTestId(TestId.post.list);
 
--- modules/post/screen/MeetingPostList.tsx
+++ modules/post/screen/MeetingPostList.tsx
@@ -1,11 +1,13 @@
-import TestId from '@shared/constants/testIds';
 import React from 'react';
+import TestId from '@shared/constants/testIds';
 import { Text, View } from 'react-native';
+import { Button } from '@rneui/themed';
 
 function MeetingPostList() {
   return (
     <View testID={TestId.post.list}>
       <Text>MeetingPostList</Text>
+      <Button title="Clear" type="clear" />
     </View>
   );
 }
