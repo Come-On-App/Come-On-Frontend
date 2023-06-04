082120c2b43d30e178ddbad0637f95711e74878d
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-05 02:41:03 +0900

    Modify: 주석 업데이트

--- App.tsx
+++ App.tsx
@@ -7,8 +7,9 @@ import RootNavigation from './modules/app/navigation/RootNavigation';
 /**
  * 목표
  * 1. 디자인 프레임 워크 도입 [v]
- * 2. 디자인 레이아웃 반응형 설계 고민 []
+ * 2. 디자인 레이아웃 반응형 설계 고민 [v]
  * 3. 레퍼 컴포넌트 유틸 알아보기 [v]
+ * 4. 스토리북 도입 고려하기 []
  */
 export default function App() {
   return (
--- modules/app/navigation/BottomTabNavigator.tsx
+++ modules/app/navigation/BottomTabNavigator.tsx
@@ -10,9 +10,9 @@ import { BottomTabParamList } from '../types/navigation';
 const { Navigator, Screen } = createBottomTabNavigator<BottomTabParamList>();
 
 /**
- * 첫번째 스크린: 모임 게시물 리스트
- * 두번째 스크린: 모임 입장
- * 세번째 스크린: 마이페이지
+ * 첫 번째 스크린: 모임 게시물 리스트
+ * 두 번째 스크린: 모임 입장
+ * 세 번째 스크린: 마이페이지
  */
 function BottomTabNavigator({ initialRouteName }: { initialRouteName?: Tab }) {
   return (
