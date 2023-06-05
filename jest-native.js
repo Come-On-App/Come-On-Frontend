581af7000dd0c142153c157f674f9b326c9b89e4
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-06 01:23:17 +0900

    Modify: 주석 업데이트

--- jest-native.js
+++ jest-native.js
@@ -1,3 +1,5 @@
-// @see https://github.com/testing-library/jest-native
+/**
+ * @see docs https://github.com/testing-library/jest-native
+ */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-native/extend-expect';
