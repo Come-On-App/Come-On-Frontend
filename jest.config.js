0fb22e466ce2c72f1652daaa2b19ca11208bcccd
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-06 02:08:31 +0900

    Add: 코드 커버리지 활성화

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui/*)',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './modules/**/*.@(js|jsx|ts|tsx)',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js',
+  ],
 };
