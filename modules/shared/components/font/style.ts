93a825430f0c787ba44c79a9a8d56cddb30490f1
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-08 23:31:01 +0900

    Add: 스타일 추가

---
+++ modules/shared/components/font/style.ts
@@ -0,0 +1,8 @@
+import { makeStyles } from '@rneui/themed';
+
+export default makeStyles((theme) => ({
+  defaultStyle: {
+    color: theme.font.grayscale['900'],
+    fontSize: theme.font.type.title4.fontSize,
+  },
+}));
