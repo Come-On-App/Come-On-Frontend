a211858d83c55e03e15924f4860e87a1b339bf66
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-09 00:38:46 +0900

    Add: Font 컴포넌트 작성

--- modules/shared/components/font/Font.tsx
+++ modules/shared/components/font/Font.tsx
@@ -4,12 +4,9 @@ import { Text } from '@rneui/themed';
 import { FontProps } from './type';
 import useStyle from './style';
 
-export default function Font({ style, children }: FontProps) {
+export default function Font({ style, children, bold }: FontProps) {
   const { defaultStyle } = useStyle();
+  const fontFamily = bold ? 'pretendard-bold' : 'pretendard-regular';
 
-  return (
-    <Text style={[defaultStyle, style, { fontFamily: 'pretendard-regular' }]}>
-      {children}
-    </Text>
-  );
+  return <Text style={[defaultStyle, style, { fontFamily }]}>{children}</Text>;
 }
