f8f9e501b9a2806c77b61cbc62b3f1bfbb57a50a
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-08 21:11:40 +0900

    Remove: 파일 삭제

--- modules/shared/components/Icon/type.ts
+++
@@ -1,9 +0,0 @@
-import { MaterialIcons } from '@expo/vector-icons';
-
-export type IconName = React.ComponentProps<typeof MaterialIcons>['name'];
-
-export interface IconProps {
-  name: IconName;
-  color: string;
-  size: number;
-}
