c998a3afcf401cdf2bfc33810a28d881d08eaca1
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-09 00:37:27 +0900

    Add: Font 컴포넌트 테스트 파일 추가

--- modules/shared/components/font/Font.spec.tsx
+++ modules/shared/components/font/Font.spec.tsx
@@ -5,14 +5,23 @@ import Font from './Font';
 import ThemeProvider from '../ThemeProvider';
 
 describe('font Compoent', () => {
-  test('올바른 스타일과 폰트 패밀리로 자식을 렌더링 해야 한다.', () => {
-    const children = 'Hello World!';
+  const children = 'Hello World!';
 
+  test('올바른 스타일과 폰트 패밀리로 자식을 렌더링 해야 한다.', () => {
     render(<Font>{children}</Font>, { wrapper: ThemeProvider });
 
     const fontElement = screen.getByText(children);
 
     expect(fontElement).toHaveStyle({ fontFamily: 'pretendard-regular' });
     expect(fontElement).toBeOnTheScreen();
+  });
+
+  test('폰트 굵기 수정이 가능해야 한다.', () => {
+    render(<Font bold>{children}</Font>, { wrapper: ThemeProvider });
+
+    const BoldFontElement = screen.getByText(children);
+
+    expect(BoldFontElement).toHaveStyle({ fontFamily: 'pretendard-bold' });
+    expect(BoldFontElement).toBeOnTheScreen();
   });
 });
