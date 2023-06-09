1aed0aad9e08f94657e67cbabdebd59a2fe21644
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-09 20:51:34 +0900

    Modify: 스토리북 렌더링 요소 수정

--- modules/post/components/card/thumbnail/Thumbnail.stories.tsx
+++ modules/post/components/card/thumbnail/Thumbnail.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react-native';
 
 import Thumbnail from './Thumbnail';
 import { FontLoader } from '@shared/components/ThemeProvider';
+import Display from '../display/Display';
 
 const path =
   'https://images.unsplash.com/photo-1682687220067-dced9a881b56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&q=80';
@@ -34,10 +35,15 @@ export const Default: Meta = {
 
 type ThumbnailStory = ComponentStory<typeof Thumbnail>;
 
+// TODO: 자식 컴포넌트화 시키기
 export const WithTopComponent: ThumbnailStory = (args) => (
   <Thumbnail uri={args.uri}>
-    <View>
-      <View style={{ height: 36, backgroundColor: 'pink' }} />
+    <View style={{ flexDirection: 'row' }}>
+      <Display name="groups">999명</Display>
+      <Display name="check-circle">확정</Display>
+      <Display name="check-circle" disabled>
+        미확정
+      </Display>
     </View>
   </Thumbnail>
 );
