d0fa3315eaef490c0a980ee69ae5fc0e25e9de62
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-09 20:49:05 +0900

    Add: Thumbnail 스타일 추가

--- modules/post/components/card/thumbnail/style.ts
+++ modules/post/components/card/thumbnail/style.ts
@@ -1,9 +1,17 @@
 import { makeStyles } from '@rneui/themed';
 
 export default makeStyles(() => ({
-  contianer: {
+  ImageContianer: {
     height: 200,
     borderRadius: 8,
     marginBottom: 8,
+  },
+  TopContainer: {
+    height: 36,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 6,
+    marginHorizontal: 12,
   },
 }));
