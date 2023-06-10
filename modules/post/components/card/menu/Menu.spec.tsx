3c23c3b82f40ee782bdbb623403f508b4258e807
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-10 14:28:26 +0900

    Add: CardMenu 컴포넌트 테스트 코드 작성

---
+++ modules/post/components/card/menu/Menu.spec.tsx
@@ -0,0 +1,15 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
+
+import { wrapper } from '@shared/components/ThemeProvider';
+import Menu from './Menu';
+
+describe('Menu Compoent', () => {
+  test('아이콘 형태의 메뉴 팝업 버튼이 렌더링 되어야 한다.', async () => {
+    render(<Menu />, wrapper);
+
+    const Component = screen.getByTestId('RNE__ICON');
+
+    expect(Component).toBeVisible();
+  });
+});
