b66b6d42b356af89e6a76b62b5690c6941a9503c
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-11 01:03:49 +0900

    Modify: Key 속성 임시 처리

--- modules/post/components/cardList/CardList.tsx
+++ modules/post/components/cardList/CardList.tsx
@@ -1,19 +1,17 @@
 import { ScrollView } from 'react-native';
 import React from 'react';
+import _ from 'lodash';
+
 import TestId from '@shared/constants/testIds';
 import { ICardList } from './type';
 import Card from '../card/Card';
 
+// TODO: key 속성 처리하기
 export default function CardList({ payloads }: ICardList) {
   return (
     <ScrollView testID={TestId.post.cardList}>
       {payloads.map((payload) => {
-        return (
-          <Card
-            payload={payload}
-            key={`${payload.people}${payload.subTitle}`}
-          />
-        );
+        return <Card payload={payload} key={`${_.random(true)}`} />;
       })}
     </ScrollView>
   );
