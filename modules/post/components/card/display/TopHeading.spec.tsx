00cd9a0c463d4d74fb62b35a09bf7cd078710b98
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-10 22:39:52 +0900

    Add: 카드 상단 컴포넌트 테스트 코드 작성

---
+++ modules/post/components/card/display/TopHeading.spec.tsx
@@ -0,0 +1,22 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
+
+import { wrapper } from '@shared/components/ThemeProvider';
+import TopHeading from './TopHeading';
+
+describe('TopHeading Compoent', () => {
+  test('주어진 속성으로 올바르게 렌더링 되어야 한다.', () => {
+    const people = 30;
+    const decided = true;
+    const notDecided = false;
+
+    render(<TopHeading people={people} isDecided={decided} />, wrapper);
+
+    expect(screen.getByText('30명')).toBeOnTheScreen();
+    expect(screen.getByText('확정')).toBeOnTheScreen();
+
+    render(<TopHeading people={people} isDecided={notDecided} />, wrapper);
+
+    expect(screen.getByText('미확정')).toBeOnTheScreen();
+  });
+});
