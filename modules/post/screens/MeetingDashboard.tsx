c0149c9e279c284c668d1b34f936628918ad2ddf
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-12 00:40:49 +0900

    Add: 모의 데이터 추가 & 적용

--- modules/post/screens/MeetingDashboard.tsx
+++ modules/post/screens/MeetingDashboard.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { View } from 'react-native';
+
 import TestId from '@shared/constants/testIds';
-import { Text, View } from 'react-native';
+import SearchAndCreateBar from '@post/components/searchAndCreate/SearchAndCreateBar';
+import CardList from '@post/components/cardList/CardList';
+import DB from './mockDB';
 
 function MeetingDashboard() {
-  return <View testID={TestId.post.list} />;
+  return (
+    <View testID={TestId.post.list}>
+      <SearchAndCreateBar />
+      <CardList payloads={DB} />
+    </View>
+  );
 }
 
 export default MeetingDashboard;
