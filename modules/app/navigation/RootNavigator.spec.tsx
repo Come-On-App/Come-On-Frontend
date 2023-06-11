d1392c0d7a6b3cd8bdae0bcc27ece0ae0b83d47d
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-12 00:09:29 +0900

    Modify: 테스트 속성 수정

--- modules/app/navigation/RootNavigator.spec.tsx
+++ modules/app/navigation/RootNavigator.spec.tsx
@@ -2,19 +2,19 @@ import { describe, expect, test } from '@jest/globals';
 import { render, screen } from '@testing-library/react-native';
 
 import TestId from '@shared/constants/testIds';
+import { wrapper } from '@shared/components/ThemeProvider';
 import RootNavigation from './RootNavigation';
-import { options } from './config';
 
 describe('<RootNavigatro />', () => {
   test('최초에 앱이 실행되면 첫 번째 하단 네비게이터가 활성화돼야 한다.', () => {
-    render(<RootNavigation />);
+    render(<RootNavigation />, wrapper);
 
     const FirstBottomTab = screen.getByRole('button', {
-      name: options.TabOne.tabBarLabel,
+      selected: true,
     });
     const Component = screen.getByTestId(TestId.post.list);
 
     expect(Component).toBeOnTheScreen();
-    expect(FirstBottomTab).toHaveAccessibilityState({ selected: true });
+    expect(FirstBottomTab).toBeTruthy();
   });
 });
