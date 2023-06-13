4855b7e0d56e43b93994860a9534ae52a1934e32
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-14 01:26:30 +0900

    Add: MeetingDashboard 테스트 코드 추가

--- modules/post/screens/MeetingDashboard.spec.tsx
+++ modules/post/screens/MeetingDashboard.spec.tsx
@@ -1,9 +1,23 @@
-import { describe, expect, test } from '@jest/globals';
-import { render, screen } from '@testing-library/react-native';
+import { describe, expect, test, jest } from '@jest/globals';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 
 import TestId from '@shared/constants/testIds';
 import { wrapper } from '@shared/components/ThemeProvider';
 import MeetingDashboard from './MeetingDashboard';
+
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav: unknown[] = jest.requireActual('@react-navigation/native');
+
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+      dispatch: jest.fn(),
+    }),
+  };
+});
 
 describe('<MeetingDashboard />', () => {
   test('모임 리스트 컴포넌트가 렌더링 되어야 한다.', () => {
@@ -12,5 +26,13 @@ describe('<MeetingDashboard />', () => {
     const Component = screen.getByTestId(TestId.post.list);
 
     expect(Component).toBeOnTheScreen();
+  });
+
+  test('MeetingPostCreation 컴포넌트로 네비게이터 되는 버튼이 있어야 한다.', () => {
+    render(<MeetingDashboard />, wrapper);
+
+    fireEvent.press(screen.getByTestId(TestId.shared.button.icon));
+
+    expect(mockedNavigate).toHaveBeenCalledWith('MeetingPostCreation');
   });
 });
