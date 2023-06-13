0d9f6bf4a7388ef2dbe1fbdec1210b6a9c9b5d73
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-14 01:27:17 +0900

    Add: 네비게이터 등록

--- modules/post/navigation/PostNavigator.tsx
+++ modules/post/navigation/PostNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { MeetingPostListParamList } from '@post/navigation/type';
 import MeetingDashboard from '@post/screens/MeetingDashboard';
+import MeetingPostCreator from '@post/screens/MeetingPostCreator';
 
 const { Screen, Navigator } =
   createNativeStackNavigator<MeetingPostListParamList>();
@@ -17,6 +18,7 @@ function PostNavigator() {
       }}
     >
       <Screen name="MeetingPostList" component={MeetingDashboard} />
+      <Screen name="MeetingPostCreation" component={MeetingPostCreator} />
     </Navigator>
   );
 }
