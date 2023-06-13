80c07c72b8e076bad1f7b87b17727ef7321e6bdf
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-14 02:05:39 +0900

    Fix: 스토리북 컴포넌트 내부 네비게이터 객체 참조 오류 이슈 수정

--- modules/post/components/searchAndCreate/SearchAndCreateBar.stories.tsx
+++ modules/post/components/searchAndCreate/SearchAndCreateBar.stories.tsx
@@ -1,8 +1,27 @@
-import { View } from 'react-native';
-import { ComponentMeta } from '@storybook/react-native';
+import { ComponentMeta, StoryFnReactReturnType } from '@storybook/react-native';
 
 import { FontLoader } from '@shared/components/ThemeProvider';
 import SearchAndCreateBar from './SearchAndCreateBar';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const StoryBookStack = createNativeStackNavigator();
+
+const NavigationDecorator = (Story: () => StoryFnReactReturnType) => {
+  const Screen = () => Story();
+
+  return (
+    <NavigationContainer independent={true}>
+      <StoryBookStack.Navigator>
+        <StoryBookStack.Screen
+          name="MeetingPostCreation"
+          component={Screen}
+          options={{ header: () => null }}
+        />
+      </StoryBookStack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 type Meta = ComponentMeta<typeof SearchAndCreateBar>;
 
@@ -10,6 +29,7 @@ export default {
   title: 'SearchAndCreateBar',
   component: SearchAndCreateBar,
   decorators: [
+    NavigationDecorator,
     (Story) => (
       <FontLoader>
         <Story />
