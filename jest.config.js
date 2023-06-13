d22fe76a442e1895670776954b1fb5a94dd9a08c
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-14 01:25:32 +0900

    Add: jest 구성 설정 추가

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui/*)',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  clearMock: true,
   collectCoverage: true,
   collectCoverageFrom: [
     './modules/**/*.@(js|jsx|ts|tsx)',
