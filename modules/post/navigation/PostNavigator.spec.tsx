7cdc4ad8e310b715c9fbbca65ec603d35885a9bc
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-14 01:26:08 +0900

    Add: PostNavigator 테스트 코드 작성

--- modules/post/navigation/PostNavigator.spec.tsx
+++ modules/post/navigation/PostNavigator.spec.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { render, screen } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import TestId from '@shared/constants/testIds';
@@ -19,5 +19,23 @@ describe('<PostNavigator />', () => {
     const Component = screen.getByTestId(TestId.post.list);
 
     expect(Component).toBeOnTheScreen();
+  });
+
+  test('모임 생성 버튼을 클릭하면 모임 생성 페이지로 네비게이터 돼야 한다.', async () => {
+    const Navigation = (
+      <NavigationContainer>
+        <PostNavigator />
+      </NavigationContainer>
+    );
+
+    render(Navigation, wrapper);
+
+    const Button = screen.getByTestId(TestId.shared.button.icon);
+
+    fireEvent.press(Button);
+
+    const Screen = await screen.findByTestId(TestId.post.creator);
+
+    expect(Screen).toBeOnTheScreen();
   });
 });
