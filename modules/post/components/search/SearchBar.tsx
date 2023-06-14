013208966902a237d23eecf783efd1b34462d8ec
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-15 02:02:32 +0900

    Refactor: 컴포넌트 분리 (재사용 컴포넌트)

--- modules/post/components/search/SearchBar.tsx
+++ modules/post/components/search/SearchBar.tsx
@@ -1,45 +1,28 @@
-import { View, Pressable } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
 import _ from 'lodash';
 
-import Font from '@shared/components/font/Font';
 import { formatDateRange } from '@shared/utils/utils';
-import Icon from '@shared/components/icon/Icon';
 import TestId from '@shared/constants/testIds';
+import PressableInput from '@shared/components/input/PressableInput';
 import useStyles from './style';
 import { ISearchBar } from './type';
 
 const INITIAL_VALUE = '모이기로 한 날짜를 검색해 보세요!';
 
 export default function SearchBar({ dateRange }: ISearchBar) {
-  const {
-    cSearchBar,
-    cInputSearchBar,
-    cSearchBarkIcon,
-    searchBarkIcon,
-    searchBarkFont,
-  } = useStyles();
+  const { searchBarkIcon } = useStyles();
 
   return (
     <View style={{ flex: 1 }}>
-      <Pressable testID={TestId.post.button.searchBar}>
-        <View style={cSearchBar}>
-          <View style={cInputSearchBar}>
-            <View style={cSearchBarkIcon}>
-              <Icon
-                name="date-range"
-                color={searchBarkIcon.color}
-                size={searchBarkIcon.size}
-              />
-            </View>
-            <Font style={searchBarkFont}>
-              {_.isEmpty(dateRange)
-                ? INITIAL_VALUE
-                : formatDateRange(dateRange)}
-            </Font>
-          </View>
-        </View>
-      </Pressable>
+      <PressableInput
+        icon={{
+          name: 'date-range',
+          color: searchBarkIcon.color,
+        }}
+        text={_.isEmpty(dateRange) ? INITIAL_VALUE : formatDateRange(dateRange)}
+        testID={TestId.post.button.searchBar}
+      />
     </View>
   );
 }
