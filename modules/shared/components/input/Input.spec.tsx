de1e3021c0021870f6f377b0f442e6606900ddbf
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-15 01:00:35 +0900

    Add: Input 테스트 코드 작성

---
+++ modules/shared/components/input/Input.spec.tsx
@@ -0,0 +1,26 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
+
+import Input from './Input';
+import { wrapper } from '../ThemeProvider';
+
+describe('Input Compoent', () => {
+  test('빈 문자열 상태에서는 placeholder 텍스트가 렌더링 되어야 한다.', () => {
+    const text = '';
+    const placeholder = '모임 이름을 입력해 주세요!';
+
+    render(<Input text={text} placeholder={placeholder} />, wrapper);
+
+    expect(
+      screen.getByPlaceholderText('모임 이름을 입력해 주세요!'),
+    ).toBeOnTheScreen();
+  });
+
+  test('전달된 텍스트가 올바르게 렌더링 되어야 한다.', () => {
+    const text = 'apple';
+
+    render(<Input text={text} />, wrapper);
+
+    expect(screen.getByDisplayValue('apple')).toBeOnTheScreen();
+  });
+});
