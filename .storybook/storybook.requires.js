da4758cb2f0a698bcbded0fa6b19578b6f35b8eb
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-14 22:01:43 +0900

    Add: 스토리북 경로 추가

--- .storybook/storybook.requires.js
+++ .storybook/storybook.requires.js
@@ -65,6 +65,7 @@ const getStories = () => {
     "./modules/post/components/searchAndCreate/SearchAndCreateBar.stories.tsx": require("../modules/post/components/searchAndCreate/SearchAndCreateBar.stories.tsx"),
     "./modules/shared/components/button/IconButton.stories.tsx": require("../modules/shared/components/button/IconButton.stories.tsx"),
     "./modules/shared/components/font/Font.stories.tsx": require("../modules/shared/components/font/Font.stories.tsx"),
+    "./modules/shared/components/imageUploader/ImageUploader.stories.tsx": require("../modules/shared/components/imageUploader/ImageUploader.stories.tsx"),
     "./modules/shared/components/menu/Menu.stories.tsx": require("../modules/shared/components/menu/Menu.stories.tsx"),
   };
 };
