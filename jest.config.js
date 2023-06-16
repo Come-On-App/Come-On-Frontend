e9a6bb2d3a326c37083f7b36bceb7d58d9b217d9
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-16 22:44:25 +0900

    Fix: 잘못된 jest.config 수정

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui/*)',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  clearMock: true,
+  clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: [
     './modules/**/*.@(js|jsx|ts|tsx)',
