0d118ec2b16167e4cb431c56c2e9ec85910b884d
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-17 15:35:36 +0900

    Add: MyPageNavigator 테스트 코드 작성

---
+++ modules/account/components/navigation/AccountNavigator.spec.tsx
@@ -0,0 +1,22 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import TestId from '@shared/constants/testIds';
+import MyPageNavigator from './AccountNavigator';
+
+describe('<MyPageNavigator />', () => {
+  test('렌더링 시 마이페이지 컴포넌트가 표시된다.', () => {
+    const Navigation = (
+      <NavigationContainer>
+        <MyPageNavigator />
+      </NavigationContainer>
+    );
+
+    render(Navigation);
+
+    const Component = screen.getByTestId(TestId.account.myPage);
+
+    expect(Component).toBeOnTheScreen();
+  });
+});
