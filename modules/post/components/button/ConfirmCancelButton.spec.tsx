8bf08dde2afc7440705cb6f98ef596fbe9456087
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-18 20:41:39 +0900

    Add: ConfirmCancelButton 테스트 케이스 추가 (#144)

--- modules/post/components/button/ConfirmCancelButton.spec.tsx
+++ modules/post/components/button/ConfirmCancelButton.spec.tsx
@@ -1,5 +1,5 @@
-import { describe, expect, test } from '@jest/globals';
-import { render, screen } from '@testing-library/react-native';
+import { describe, expect, jest, test } from '@jest/globals';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 
 import TestId from '@shared/constants/testIds';
 import { wrapper } from '@shared/components/ThemeProvider';
@@ -7,8 +7,35 @@ import ConfirmCancelButton from './ConfirmCancelButton';
 
 describe('ConfirmCancelButton Compoent', () => {
   test('두 개의 버튼 컴포넌트가 렌더링 되어야 한다.', () => {
-    render(<ConfirmCancelButton />, wrapper);
+    render(
+      <ConfirmCancelButton
+        onCancelHandler={jest.fn()}
+        onConfirmlHandler={jest.fn()}
+      />,
+      wrapper,
+    );
 
     expect(screen.getAllByTestId(TestId.shared.button.default)).toHaveLength(2);
+  });
+
+  test('각각의 핸들러는 이벤트에 반응해야 한다.', () => {
+    const onCancelHandler = jest.fn();
+    const onConfirmlHandler = jest.fn();
+
+    render(
+      <ConfirmCancelButton
+        onCancelHandler={onCancelHandler}
+        onConfirmlHandler={onConfirmlHandler}
+      />,
+      wrapper,
+    );
+
+    fireEvent.press(screen.getByText('취소'));
+
+    expect(onCancelHandler).toHaveBeenCalledTimes(1);
+
+    fireEvent.press(screen.getByText('완료'));
+
+    expect(onConfirmlHandler).toHaveBeenCalledTimes(1);
   });
 });
