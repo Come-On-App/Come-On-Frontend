6856df36b6fe5e83633029cd0097f208c2d3640e
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-18 20:30:04 +0900

    Add: 주석 추가

--- modules/shared/components/layout/Layout.tsx
+++ modules/shared/components/layout/Layout.tsx
@@ -1,8 +1,12 @@
 import { View } from 'react-native';
 import React from 'react';
+
 import { IscreenLayout } from './type';
 import useStyles from './style';
 
+/**
+ * 페이지 스크린 레이아웃
+ */
 export default function ScreenLayout({ children, testID }: IscreenLayout) {
   const { container } = useStyles();
 
