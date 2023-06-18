cb0827f31778e58b6333b89bd4d8ec7d35b37184
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-18 20:18:39 +0900

    Add: 유틸 함수 truncateText 추가

--- modules/shared/utils/utils.ts
+++ modules/shared/utils/utils.ts
@@ -1,8 +1,17 @@
 import { IFormatDateRange } from './type';
 
-// eslint-disable-next-line import/prefer-default-export
 export function formatDateRange(range: IFormatDateRange) {
   return Object.values(range)
     .map((date) => date.split('-').join('.'))
     .join(' ~ ');
+}
+
+export function truncateText(maxLength: number) {
+  return (currentText: string) => {
+    if (currentText.length > maxLength) {
+      return currentText.slice(0, maxLength);
+    }
+
+    return currentText;
+  };
 }
