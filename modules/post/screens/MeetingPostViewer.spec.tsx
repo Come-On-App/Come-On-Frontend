5b7a42341c940dc960a9de9889c95298ac0ecf8d
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-22 23:18:12 +0900

    Add: MeetingPostDetail 네비게이터 & MeetingPostViewer 컴포넌트 스토리북 적용 (#146)

---
+++ modules/post/screens/MeetingPostViewer.spec.tsx
@@ -0,0 +1,12 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
+
+import MeetingPostViewer from './MeetingPostViewer';
+
+describe('MeetingPostViewer Compoent', () => {
+  test.todo('모임 멤버 관련 테스트');
+
+  test.todo('모임 기관 관련 테스트');
+
+  test.todo('모임 장소 관련 테스트');
+});
