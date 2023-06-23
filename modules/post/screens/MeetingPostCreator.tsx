762f6ec336edd691fb5d0cb03db86799bc9b6302
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-23 12:00:39 +0900

    Refactor: 파일명 수정

--- modules/post/screens/MeetingPostCreator.tsx
+++ modules/post/screens/MeetingPostCreator.tsx
@@ -4,7 +4,7 @@ import TestId from '@shared/constants/testIds';
 import MeetingNameInput from '@post/components/creation/meetingNameInput/MeetingNameInput';
 import Uploader from '@post/components/creation/uploader/Uploader';
 import VotingTimeRangePicker from '@post/components/creation/votingTimeRangePicker/VotingTimeRangePicker';
-import ScreenLayout from '@shared/components/layout/Layout';
+import ScreenLayout from '@shared/components/layout/ScreenLayout';
 import ConfirmCancelButton from '@post/components/button/ConfirmCancelButton';
 import useStyles from './style';
 
