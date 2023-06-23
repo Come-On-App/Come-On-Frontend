bd68f6c69fd37e66ae4ca708f04b14d863a33ab8
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-23 12:08:24 +0900

    Refactor: BottomTabNavigator 함수분리 (#146)

--- modules/app/navigation/BottomTabNavigator.tsx
+++ modules/app/navigation/BottomTabNavigator.tsx
@@ -5,7 +5,7 @@ import MyPageNavigator from '@account/navigation/AccountNavigator';
 import MeetingCode from '@connection/screen/MeetingCode';
 import PostNavigator from '@post/navigation/PostNavigator';
 import { Tab, options } from './config';
-import { BottomTabParamList } from './type';
+import { BottomTabParamList, IbottomTabNavigator, Inavigation } from './type';
 
 const { Navigator, Screen } = createBottomTabNavigator<BottomTabParamList>();
 
@@ -14,7 +14,10 @@ const { Navigator, Screen } = createBottomTabNavigator<BottomTabParamList>();
  * 두 번째 스크린: 모임 입장
  * 세 번째 스크린: 마이페이지
  */
-function BottomTabNavigator({ initialRouteName }: { initialRouteName?: Tab }) {
+function BottomTabNavigator({
+  children,
+  initialRouteName,
+}: IbottomTabNavigator) {
   return (
     <Navigator
       initialRouteName={initialRouteName}
@@ -22,6 +25,14 @@ function BottomTabNavigator({ initialRouteName }: { initialRouteName?: Tab }) {
         headerShown: false,
       }}
     >
+      {children}
+    </Navigator>
+  );
+}
+
+export default function Navigation({ initialRouteName }: Inavigation) {
+  return (
+    <BottomTabNavigator initialRouteName={initialRouteName}>
       <Screen
         name={Tab.one}
         component={PostNavigator}
@@ -37,8 +48,6 @@ function BottomTabNavigator({ initialRouteName }: { initialRouteName?: Tab }) {
         component={MyPageNavigator}
         options={options[Tab.three]}
       />
-    </Navigator>
+    </BottomTabNavigator>
   );
 }
-
-export default BottomTabNavigator;
