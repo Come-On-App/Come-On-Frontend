21858c7e0530677ecf54e64e018815a2749d7efe
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-24 19:29:17 +0900

    Modify: Schedule 테스트 케이스 수정 & 추가 (#146)

--- modules/post/components/detail/duration/schedule/Schedule.spec.tsx
+++ modules/post/components/detail/duration/schedule/Schedule.spec.tsx
@@ -4,17 +4,32 @@ import { render, screen } from '@testing-library/react-native';
 import Schedule from './Schedule';
 
 describe('Schedule Compoent', () => {
-  test('날짜가 확정되지 않으면 관련 날짜 범위와 관련된 텍스트를 렌더링 되어야 한다.', () => {
-    const range = {
-      startFrom: '2023-06-10',
-      endTo: '2023-06-20',
-    };
+  const range = {
+    startFrom: '2023-06-10',
+    endTo: '2023-06-20',
+  };
 
+  test('날짜형식이 올바르게 포맷팅되어 렌더링 되어야 한다.', () => {
     render(<Schedule range={range} isFixed={false} />);
 
     expect(
-      screen.getByText('2022년 06월 10일 ~ 2022년 06월 20일'),
+      screen.getByText('2023년 06월 10일 ~ 2023년 06월 20일'),
     ).toBeOnTheScreen();
+  });
+
+  test('날짜 범위가 동일하다면 하나의 날짜 범위만 포맷팅되어 렌더링 되어야 한다.', () => {
+    const equalRange = {
+      startFrom: '2023-06-10',
+      endTo: '2023-06-10',
+    };
+
+    render(<Schedule range={equalRange} isFixed={false} />);
+
+    expect(screen.getByText('2023년 06월 10일')).toBeOnTheScreen();
+  });
+
+  test('날짜가 확정되지 않으면 관련 날짜 범위와 관련된 텍스트를 렌더링 되어야 한다.', () => {
+    render(<Schedule range={range} isFixed={false} />);
 
     expect(
       screen.getByText('참석 가능 날짜에 투표해 보세요!'),
@@ -22,14 +37,7 @@ describe('Schedule Compoent', () => {
   });
 
   test('날짜가 확정된다면 관련 날짜 범위와 관련된 텍스트를 렌더링 되어야 한다.', () => {
-    const range = {
-      startFrom: '2023-06-10',
-      endTo: '2023-06-20',
-    };
-
-    render(<Schedule range={range} isFixed={false} />);
-
-    expect(screen.getByText('2022년 06월 10일')).toBeOnTheScreen();
+    render(<Schedule range={range} isFixed />);
 
     expect(screen.getByText('날짜가 확정되었습니다!')).toBeOnTheScreen();
   });
