50b69eb164180ec40717cde61a6e1fc9c276adee
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-24 14:04:28 +0900

    Add: TimePickerButton 컴포넌트 & 스토리북 적용 (#146)

---
+++ modules/post/components/detail/duration/timePickerButton/TimePickerButton.stories.tsx
@@ -0,0 +1,32 @@
+import { View } from 'react-native';
+import { ComponentMeta } from '@storybook/react-native';
+
+import { FontLoader } from '@shared/components/ThemeProvider';
+import Component from './TimePickerButton';
+
+type Meta = ComponentMeta<typeof Component>;
+
+export default {
+  title: 'Detail - Duration',
+  component: Component,
+  decorators: [
+    (Story) => (
+      <FontLoader>
+        <View style={{ margin: 10 }}>
+          <Story />
+        </View>
+      </FontLoader>
+    ),
+  ],
+  argTypes: {
+    isHost: {
+      type: 'boolean',
+    },
+  },
+} as Meta;
+
+export const TimePickerButton: Meta = {
+  args: {
+    time: '12:00:00',
+  },
+};
