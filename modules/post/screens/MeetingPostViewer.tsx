4783894e00ea8d6c1ee071f6483ab01f7935d08a
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-25 19:42:39 +0900

    Add: Planner 컴포넌트 & 스토리북 적용 (#146)

--- modules/post/screens/MeetingPostViewer.tsx
+++ modules/post/screens/MeetingPostViewer.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import React from 'react';
+
 import TestId from '@shared/constants/testIds';
+import Duration from '@post/components/detail/duration/Duration';
+import Participants from '@post/components/detail/participants/Participants';
+import Planner from '@post/components/detail/planner/Planner';
+import mockMembers from '@post/mocks/members';
+import mockVenueList from '@post/mocks/venueList';
+import mockDuration from '@post/mocks/duration';
 
 export default function MeetingPostViewer() {
   return (
-    <View testID={TestId.post.detail}>
-      <Text>MeetingPostViewer</Text>
-    </View>
+    <ScrollView testID={TestId.post.detail}>
+      <Participants users={mockMembers} />
+      <Duration range={mockDuration.range} time={mockDuration.time} />
+      <Planner venueList={mockVenueList} />
+    </ScrollView>
   );
 }
