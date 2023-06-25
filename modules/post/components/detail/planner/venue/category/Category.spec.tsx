ce26876c2fad4f99a6b73e9d050c1cb74971b3c4
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-25 11:40:50 +0900

    Add: Category 테스트 코드 작성 (#146)

---
+++ modules/post/components/detail/planner/venue/category/Category.spec.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
+
+import { wrapper } from '@shared/components/ThemeProvider';
+import Category from './Category';
+
+describe('Category Compoent', () => {
+  test('카테고리가 올바르게 렌더링 되어야 한다.', () => {
+    render(<Category type="관광명소" />, wrapper);
+
+    expect(screen.getByText('관광명소')).toBeOnTheScreen();
+  });
+});
