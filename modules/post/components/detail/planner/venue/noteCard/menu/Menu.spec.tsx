5e590678affa995dabc3d05de8925b521badfff5
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-25 15:48:14 +0900

    Add: NoteCardMenu 테스트 코드 작성 (#146)

---
+++ modules/post/components/detail/planner/venue/noteCard/menu/Menu.spec.tsx
@@ -0,0 +1,15 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
+
+import { wrapper } from '@shared/components/ThemeProvider';
+import NoteCardMenu from './Menu';
+
+describe('NoteCardMenu Compoent', () => {
+  test('아이콘 형태의 메뉴 팝업 버튼이 렌더링 되어야 한다.', () => {
+    render(<NoteCardMenu />, wrapper);
+
+    const Component = screen.getByTestId('RNE__ICON');
+
+    expect(Component).toBeVisible();
+  });
+});
