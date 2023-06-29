2cad9ad880cd4880b703a2d5924c76b4a8d508b5
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-30 00:35:10 +0900

    Refactor: 로직 리팩토링

--- modules/account/components/description/style.ts
+++ modules/account/components/description/style.ts
@@ -1,11 +1,21 @@
 import { makeStyles } from '@rneui/themed';
+import { applyRelativeSizes } from '@shared/utils/utils';
 
-export default makeStyles((theme) => ({
-  emailFont: {
-    fontSize: theme.font.type.body1.fontSize,
-    color: theme.font.grayscale['500'],
-  },
-  welcomeFont: {
-    fontSize: theme.font.type.title3.fontSize,
-  },
-}));
+export default makeStyles((theme) => {
+  const [EMAIL_FONT_SIZE, WELCOME_FONT_SIZE] = applyRelativeSizes(
+    Object.values({
+      emailFontSize: theme.font.type.body1.fontSize,
+      welcomeFontSize: theme.font.type.title3.fontSize,
+    }),
+  );
+
+  return {
+    emailFont: {
+      fontSize: EMAIL_FONT_SIZE,
+      color: theme.font.grayscale['500'],
+    },
+    welcomeFont: {
+      fontSize: WELCOME_FONT_SIZE,
+    },
+  };
+});
