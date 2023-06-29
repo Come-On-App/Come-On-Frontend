f059346f942e306f95203d0f569cf352e6da4b16
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-29 21:26:57 +0900

    Add: UserAvatar 테스트 코드 추가 (#152)

---
+++ modules/account/components/userAvatar/UserAvatar.spec.tsx
@@ -0,0 +1,18 @@
+import { describe, expect, test } from '@jest/globals';
+import { screen } from '@testing-library/react-native';
+
+import TestId from '@shared/constants/testIds';
+import { render } from '@shared/components/ThemeProvider';
+import { mockUserAvatarImage } from '@account/mocks/userAvatarImage';
+import UserAvatar from './UserAvatar';
+
+describe('UserAvatar Compoent', () => {
+  test('사용자 아바타 컴포펀트가 올바르게 렌더링 되어야 한다.', () => {
+    render(<UserAvatar path={mockUserAvatarImage} />);
+
+    expect(screen.getByTestId(TestId.account.avatar)).toBeOnTheScreen();
+    expect(screen.getByTestId(TestId.shared.avatar.badge)).toBeOnTheScreen();
+  });
+
+  test.todo('아바타를 클릭하면 이벤트가 올바르게 발생해야 한다.');
+});
