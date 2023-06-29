db2c47f0687e68bf588b20e36f6f29bcbf0a4cde
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-29 14:18:14 +0900

    Add: BadgedAvatar 테스트 코드 추가 (#152)

--- modules/shared/components/avatar/Avatar.spec.tsx
+++ modules/shared/components/avatar/Avatar.spec.tsx
@@ -1,16 +1,28 @@
 import { describe, expect, test } from '@jest/globals';
 import { render, screen } from '@testing-library/react-native';
 
-import Avatar from './Avatar';
+import TestId from '@shared/constants/testIds';
+import Avatar, { BadgedAvatar } from './Avatar';
+import { wrapper } from '../ThemeProvider';
 
 describe('Avatar Component', () => {
   test('주어진 속성으로 올바르게 렌더링 해야한다.', () => {
     const uri = 'https://image...';
 
-    render(<Avatar path={uri} />);
+    render(<Avatar path={uri} />, wrapper);
 
     expect(screen.getByTestId('RNE__Avatar__Image')).toHaveProp('source', {
       uri,
     });
+  });
+});
+
+describe('BadgedAvatar Component', () => {
+  test('뱃지 아이콘이 렌더링 되어야 한다.', () => {
+    const uri = 'https://image...';
+
+    render(<BadgedAvatar badgeName="photo-camera" path={uri} />, wrapper);
+
+    expect(screen.getByTestId(TestId.shared.avatar.badge)).toBeOnTheScreen();
   });
 });
