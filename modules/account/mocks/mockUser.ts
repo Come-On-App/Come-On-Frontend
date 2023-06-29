bbdafb6594f7bd80e5ab7e0bbf999e9318f596da
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-30 00:36:28 +0900

    Add: mock 데이터 추가 (#152)

---
+++ modules/account/mocks/mockUser.ts
@@ -0,0 +1,7 @@
+import { mockUserAvatarImage } from './userAvatarImage';
+
+export const mockUser = {
+  userName: 'jeongbaebang',
+  email: 'jeongbaebang_dev@naver.com',
+  userImage: mockUserAvatarImage,
+};
