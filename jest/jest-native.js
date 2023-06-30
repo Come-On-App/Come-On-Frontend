d58516d4a6ad233e272f7288fb5d48cd25f69dfd
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-30 21:14:26 +0900

    Fix: react-native-maps 테스트 이슈 수정

--- jest/jest-native.js
+++ jest/jest-native.js
@@ -1,5 +1,5 @@
 /**
  * @see docs https://github.com/testing-library/jest-native
  */
-// eslint-disable-next-line import/no-extraneous-dependencies
+
 import '@testing-library/jest-native/extend-expect';
