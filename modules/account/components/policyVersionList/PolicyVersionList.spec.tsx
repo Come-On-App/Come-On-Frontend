e412875bcbb5a3f0d2faff8a35199905036f6e70
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-06-30 21:09:56 +0900

    Add: PolicyVersionList 테스트 코드 추가 (#152)

---
+++ modules/account/components/policyVersionList/PolicyVersionList.spec.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, test } from '@jest/globals';
+import { screen } from '@testing-library/react-native';
+
+import { render } from '@shared/components/ThemeProvider';
+import PolicyVersionList from './PolicyVersionList';
+
+describe('PolicyVersionList Compoent', () => {
+  test('요소들이 올바르게 렌더링 되어야 한다.', () => {
+    render(<PolicyVersionList />);
+
+    expect(screen.getAllByTestId('RNE__Divider')).toHaveLength(2);
+  });
+});
