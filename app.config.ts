ebec18218724d709243a0f36ce5ba27a80f63cab
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-07-15 15:44:29 +0900

    Add: 이미지 권한 문구 (#144)

--- app.config.ts
+++ app.config.ts
@@ -26,4 +26,13 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: '#ffffff',
     },
   },
+  plugins: [
+    [
+      'expo-image-picker',
+      {
+        photosPermission:
+          '사용자가 앱에서 이미지를 업로드하기 위해선, 사용자의 사진 라이브러리에 접근할 수 있어야 합니다. 이를 위해 사진 접근 권한이 필요합니다. 권한을 허용하면 사용자의 프로필 이미지를 표시하거나 모임 등록 이미지에 사용할 수 있습니다.',
+      },
+    ],
+  ],
 });
